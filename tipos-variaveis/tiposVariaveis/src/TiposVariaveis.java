79d7aba2005f9e44a9e8dc771c9a73e8806f91f5
tiago <fastmetal89@gmail.com>
2023-10-01 14:07:35 -0300

    Sistema smart tv

--- tipos-variaveis/tiposVariaveis/src/TiposVariaveis.java
+++ tipos-variaveis/tiposVariaveis/src/TiposVariaveis.java
@@ -1,16 +1,19 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        
-        double salarioMinimo =  2500;
-        short numeroCurto = 1;
-        int numeroNormal = numeroCurto;
-        short numeroCurto2 = (short) numeroNormal;
-        
-        int numero = 1;
-        numero = 5;
-        System.out.println(numero);
 
-        //constante
-        final double VALOR_DE_PI = 3.14;
+        boolean condicao1 = true;
+boolean condicao2 = false;
+
+if(condicao1 && condicao2){
+	System.out.println("as duas condições são verdadeiras");
+}
+if(condicao1 && (7 > 4)){
+    System.out.println("as duas condiçoes sao verdadeiras");
+}
+if(condicao1 || condicao2){
+System.out.println("uma das condições é verdadeira");
+}
+
+System.out.println("fim");
     }
 }
